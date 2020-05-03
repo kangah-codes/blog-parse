f3543398def17cd7dfc48f0300da55409b91d54e
Joshua Akangah <akangah89@hmail.com>
2020-05-03 02:30:22 +0000

    Working, still need to add the tables and lists

--- main.py
+++ main.py
@@ -2,73 +2,92 @@
 Parser for taking text and returning html
 
 So for instance a text like:
-	!(h1)[Hello world] will return <h1>Hello world</>
+	!h1{Hello world} will return <h1>Hello world</>
+	!img{hello.png} will return <img src="hello.png">
 
 """
 import re
 from re import split
 
 class Parser:
+	"""
+	Class for all objects to be oarsed
+	"""
 	def __init__(self, text):
 		self.text = text
 		self.length = len(self.text)
-		self.headings = []
-		self.paragraphs = []
-		self.quotes = []
-		self.tables = []
-		self.lists = []
-		self.images = []
-		self.links = []
 
 	def parse_headings(self, text):
-		return re.sub(r'!(h\d){(.*)}', r'<\1>\2</\1>', text)
+		#return re.sub(r'!(h\d){(.+)}', r'<\1>\2</\1>', text)
+		try:
+			# replacing all text with this pattern in the main string
+			self.text = re.sub(r'!(h\d){(.+)}', r'<\1>\2</\1>', text)
+		except Exception as e:
+			return False
+		else:
+			return True
 
 	def parse_paragraphs(self, text):
-		return re.sub(r'!p{(.*)}', r'<p>\1</p>', text)
+		#return re.sub(r'!p{(.+)}', r'<p>\1</p>', text)
+		try:
+			# replacing all text with this pattern in the main string
+			self.text = re.sub(r'!p{(.+)}', r'<p>\1</p>', text)
+		except Exception as e:
+			return False
+		else:
+			return True
+
 
 	def parse_quotes(self, text):
-		return re.sub(r'!quote_(h\d){(.*)}', r'<blockquote><\1>\2</\1></blockquote>', text)
+		#return re.sub(r'!quote_(h\d){(.+)}', r'<blockquote><\1>\2</\1></blockquote>', text)
+		try:
+			# replacing all text with this pattern in the main string
+			self.text = re.sub(r'!quote_(h\d){(.+)}', r'<blockquote><\1>\2</\1></blockquote>', text)
+		except Exception as e:
+			return False
+		else:
+			return True
 
 	def parse_tables(self, text):
-		"""
-		<table>
-		<thead>
-		<tr>
-		<th>Name</th>
-		<th>Age</th>
-		</tr>
-		</thead>
-		<tbody>
-		<tr>
-		<td>Bob</td>
-		<td>27</td>
-		</tr>
-		<tr>
-		<td>Alice</td>
-		<td>23</td>
-		</tr>
-		</tbody>
-		</table>
-		!table_th{Name}_th{Age}_td{Lmao}_td{Lmao}
-		"""
-		return re.sub(r'!table_(th{.*}){.}', r'lol', text)
+		#return re.sub(r'!table_(th{.*}){.}', r'lol', text)
+		try:
+			# replacing all text with this pattern in the main string
+			self.text = re.sub(r'!table_(th{.*}){.}', r'lol', text)
+		except Exception as e:
+			return False
+		else:
+			return True
 
 	def parse_lists(self, text, typeof):
-		if typeof == 'ol':
-			#print(re.findall(r'!ol_li{(.*)}', text))
-			print(re.sub(r'!ol_(li{(.*)})|(li{(.*)})', r'<ol><li>\2</li></ol>', text))
+		pass
 
+	def parse_images(self, text):
+		#return re.sub(r'!img{(.+)}', r'<img src="\1">', text)
+		try:
+			# replacing all text with this pattern in the main string
+			self.text = re.sub(r'!img{(.+)}', r'<img src="\1">', text)
+		except Exception as e:
+			return False
+		else:
+			return True
 
-a = """
-!ol_li{YEAH}_li{lmao}
-!ol_li{OKAY}
-!ol_li{lol}
+	def parse_links(self, text, make_bold=True):
+		try:
+			# replacing all text with this pattern in the main string
+			if make_bold:
+				self.text = re.sub(r'!link{(.+)}{(.+)}', r'<b><a href="\1">\2</a></b>', text)
+			else:
+				self.text = re.sub(r'!link{(.+)}{(.+)}', r'<a href="\1">\2</a>', text)
+		except Exception as e:
+			return False
+		else:
+			return True
 
-"""
-
-p = Parser(a)
-
-print(p.parse_lists(a, 'ol'))
-
-
+	def __repr__(self):
+		return f"""
+			Parser object with text {self.text}
+			Parsed headings {self.parse_headings(self.text)}
+			Parsed Paragraphs {self.parse_paragraphs(self.text)}
+			Parsed quotes {self.parse_quotes(self.text)}
+		"""
 							
